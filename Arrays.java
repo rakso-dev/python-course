f6b40e7078f9251a8b7c18008ed7093defdc98b7
Rakso <oskarr.magana@gmail.com>
2022-09-29 23:53:13 -0500

    first commit2

--- Arrays.java
+++ Arrays.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String[] args) {
-        int [] arr =  new int [5];
+        int [] arr =  {5,3,9,11,16};
         for(int i = 0; i < arr.length; i ++){
             System.out.println(arr[i]);
         }
